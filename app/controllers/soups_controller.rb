9c10d2152ce5d1fd7ce0ac4f309b4bc64e47acd9
CarolPC <carolina.pascale.c@gmail.com>
2017-01-11 22:18:00 -0200

    Adds soup response in json for show and index actions

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,8 +1,18 @@
 class SoupsController < ApplicationController
     def index
+        @soups = Soup.all
+
+        respond_to do |format|
+            format.json { render json: @soups }
+        end
     end
 
     def show
         @soup = Soup.find(params[:id])
+
+        respond_to do |format|
+            format.html
+            format.json { render json: @soup }
+        end
     end
 end
